c1dedadd6d5aff58a844892b2b0846536c937365
Pedro Boccaletti <boccaletti.pedro@gmail.com>
2024-12-28 20:53:19 -0300

    change shapeDelete, checkShapeCanMove, and add function to get shapeCells

--- shape.c
+++ shape.c
@@ -49,59 +49,73 @@ void moveShape(char** matrix, int** shapeCells, char dummy, int direction) {
         int rowTowards = row + directions[direction][0];
         int colTowards = col + directions[direction][1];
         matrix[rowTowards][colTowards] = dummy;
-        free(shapeCells[i]);
     }
 }
 
-// run the shape and checks if the move is valid
-int checkShapeCanMove(char** matrix, int i, int j, char dummy, int direction, int** shapeCells) {
+void deleteShapeCells(int** shapeCells) {
+    for (int i = 0; i < MATRIX_ROWS * MATRIX_COLS; i++) {
+        if (shapeCells[i] == NULL) {
+            break;
+        }
+        free(shapeCells[i]);
+    }
+    free(shapeCells);
+}
+
+void getShapeCells(char** matrix, int i, int j, char dummy, int** shapeCells) {
+    if (shapeCells == NULL) {
+        return;
+    }
     if (matrix[i][j] != dummy) {
-        if (DEBUG > 1) printf("debug {i:%d, j:%d, dummy:%c, direction:%d} retornara -1\n", i, j, dummy, direction);
-        return -1; // it's not a part of the shape
+        return;
     }
     for (int k = 0; k < MATRIX_ROWS * MATRIX_COLS; k++) {
-        if (shapeCells[k] && shapeCells[k][0] == i && shapeCells[k][1] == j) {
-            return -1; // already visited
-        }
         if (shapeCells[k] == NULL) {
             shapeCells[k] = (int*) malloc(2 * sizeof(int));
             if (shapeCells[k] == NULL) {
-                printf("erro alocação");
+                if (DEBUG) { printf("problema alocando shapeCells[k]"); }
+                return;
             }
             shapeCells[k][0] = i;
             shapeCells[k][1] = j;
             break;
         }
     }
-    // check neighbor cells
     for (int k = 0; k < 4; k++) {
-        int neighborRow = i + directions[k][0];
-        int neighborCol = j + directions[k][1];
-        // if not a part of the board, don't check it
-        if (!checkBoundaries(neighborRow, neighborCol)) {
+        int row = i + directions[k][0];
+        int col = j + directions[k][1];
+        if (checkCellBeenVisited(shapeCells, row, col)) {
             continue;
         }
-        if (checkCellBeenVisited(shapeCells, neighborRow, neighborCol)) {
-            continue;
+        if (checkBoundaries(row, col)) {
+            getShapeCells(matrix, row, col, dummy, shapeCells);
         }
-        int a = checkShapeCanMove(matrix, neighborRow, neighborCol, dummy, direction, shapeCells);
-        if (!a) {
-            if (DEBUG > 1) printf("debug {i:%d, j:%d, dummy:%c, direction:%d} retornara 0\n", i, j, dummy, direction);
+    }
+}
+
+int checkShapeCanMove(char** matrix, int i, int j, int direction) {
+    int** shapeCells = allocShapeCells();
+    if (shapeCells == NULL) {
+        if (DEBUG) { printf("problema alocando shapeCells"); }
+        return 0;
+    }
+    char dummy = matrix[i][j];
+    getShapeCells(matrix, i, j, dummy, shapeCells);
+    for (int k = 0; k < MATRIX_ROWS * MATRIX_COLS; k++) {
+        if (shapeCells[k] == NULL) {
+            break;
+        }
+        int rowTowards = shapeCells[k][0] + directions[direction][0];
+        int colTowards = shapeCells[k][1] + directions[direction][1];
+        if (!checkBoundaries(rowTowards, colTowards)) {
+            deleteShapeCells(shapeCells);
             return 0;
         }
-
-    }
-
-    int rowTowards = i + directions[direction][0];
-    int colTowards = j + directions[direction][1];
-    if (checkBoundaries(rowTowards, colTowards)) {
-        if (matrix[rowTowards][colTowards] == BLANK_SPACE || matrix[rowTowards][colTowards] == dummy) {
-            if (DEBUG > 1) printf("debug {i:%d, j:%d, dummy:%c, direction:%d} retornara 1\n", i, j, dummy, direction);
-
-            return 1; // can move in that direction
+        if (matrix[rowTowards][colTowards] != BLANK_SPACE && matrix[rowTowards][colTowards] != dummy) {
+            deleteShapeCells(shapeCells);
+            return 0;
         }
     }
-    // can't either because its on the edge (first if) or it is not a white space nor the shape itself
-    if (DEBUG > 1) printf("debug {i:%d, j:%d, dummy:%c, direction:%d} retornara 0\n", i, j, dummy, direction);
-    return 0;
+    deleteShapeCells(shapeCells);
+    return 1;
 }
--- shape.h
+++ shape.h
@@ -5,10 +5,11 @@
 #ifndef SHAPE_H
 #define SHAPE_H
 
-int checkShapeCanMove(char** matrix, int i, int j, char dummy, int direction, int** shapeCells);
 
 int** allocShapeCells();
-
 void moveShape(char** matrix, int** shapeCells, char dummy, int direction);
+void deleteShapeCells(int** shapeCells);
+int checkShapeCanMove(char** matrix, int i, int j, int direction);
+void getShapeCells(char** matrix, int i, int j, char dummy, int** shapeCells);
 
 #endif //SHAPE_H
