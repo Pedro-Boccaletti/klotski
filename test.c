482291c80c63467f8f4596e11cefe0d12c335476
Pedro Boccaletti <boccaletti.pedro@gmail.com>
2024-12-29 00:31:36 -0300

    changes the test file to accommodate past changes

--- test.c
+++ test.c
@@ -3,23 +3,21 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "matrix.h"
+#include "shape.h"
+#include "main.h"
 
 
-int checkBoundaries(int row, int col);
-int checkShapeCanMove(char** matrix, int i, int j, char dummy, int direction, int** shapeCells);
-int* checkMovesFromAllDirections(char** matrix, int i, int j);
-int** allocShapeCells();
-void checkWhiteSpaces(char** matrix, int rows, int cols);
-
-
-int test() {
+int test(char** testedMatrix) {
     const char testMatrix2[3][3] = {
         {'B', 'x', 'x'},
         {'R', BLANK_SPACE, BLANK_SPACE},
         {'R', 'T', 'I'},
     };
+
+    int wanted[3] = {'T', 1, 2};
 
     char** matrix2 = allocMatrix(3, 3);
     for (int i = 0; i < 3; i++) {
@@ -31,7 +29,7 @@ int test() {
     int rShape = 2;
     int cShape = 0;
     char shape = matrix2[rShape][cShape];
-    printf("%c\n", shape);
+    //printf("%c\n", shape);
     /*
     for (int i = 0; i < 4; i++) {
         int** shapeCells = allocShapeCells();
@@ -39,7 +37,12 @@ int test() {
         printf("%d\n", b);
     }
     */
-    checkMovesFromAllDirections(matrix2, 1, 1);
-    checkWhiteSpaces(matrix2, 3, 3);
+    //checkMovesFromAllDirections(matrix2, 1, 1, testedMatrix, wanted);
+    //return 0;
+    if (!checkWantedAllowed(wanted)) {
+        return 1;
+    }
+    int stackIndex = 0;
+    checkWhiteSpaces(matrix2, testedMatrix, &stackIndex, wanted, 0);
     return 0;
 }
